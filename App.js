6b8cfb740b403d48b5746bfbd547143c25ed7dc1
Martin Ruiz <jmruiz@sciodev.com>
2019-11-15 09:14:08 -0600

    Added navigation to the app following the v3+ version of react navigation

--- App.js
+++ App.js
@@ -6,109 +6,40 @@
  * @flow
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, { Component } from 'react';
+import Tabs from './src'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
+    state = {
+        cities: []
+    }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+    addCity = (city) => {
+        const cities = this.state.cities
+        cities.push(city)
+        this.setState({ cities })
+    }
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+    addLocation = (location, city) => {
+        const index = this.state.cities.findIndex(item =>{
+            return item.id === city.id
+        })
+        const chosenCity = this.state.cities[index]
+        chosenCity.locations.push(location)
+        const cities = [...this.state.cities.slice(0, index), chosenCity, ...this.state.cities.slice(index + 1)]
+        this.setState({
+            cities
+        })
+    }
 
-export default App;
+    render() {
+        return (
+            <Tabs
+                screenProps={{
+                    cities: this.state.cities,
+                    addCity: this.addCity, 
+                    addLocation: this.addLocation
+                }} />
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import Cities from './Cities/Cities'
+import City from './Cities/City'
+import AddCity from './AddCity/AddCity'
+
+import { colors } from './theme'
+
+import { createAppContainer } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createStackNavigator } from 'react-navigation-stack'
+
+const options = {
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: colors.primary
+        },
+        headerTintColor: '#fff'
+    }
+}
+
+const CitiesNav = createStackNavigator({
+    Cities: { screen: Cities },
+    City: { screen: City }
+}, options)
+
+const Tabs = createBottomTabNavigator({
+    Cities: { screen: CitiesNav },
+    AddCity: { screen: AddCity }
+})
+
+export default createAppContainer(Tabs)
